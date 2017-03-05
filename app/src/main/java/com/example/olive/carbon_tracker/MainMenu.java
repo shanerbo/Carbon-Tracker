7b0ee1d361f4840b31fdca764fd772f99a413837
ncfung <ncfung@sfu.ca>
2017-03-04 17:24:17 -0800

    Add switch-case to OnClickListener (requires other activities)

--- app/src/main/java/com/example/olive/carbon_tracker/MainMenu.java
+++ app/src/main/java/com/example/olive/carbon_tracker/MainMenu.java
@@ -1,5 +1,6 @@
 package com.example.olive.carbon_tracker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -16,12 +17,25 @@ public class MainMenu extends AppCompatActivity {
         setButton(R.id.btnCurrentFootprint);
     }
 
-    private void setButton(int id) {
+    private void setButton(final int id) {
         Button button = (Button) findViewById(id);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent showActivity = new Intent();
+                switch (id) {
+                    //TODO: Enter the names of the other Activity classes
+                    case R.id.btnCreateJourney:
+                        //showActivity = new Intent(MainMenu.this, );
+                        break;
+                    case R.id.btnSelectJourney:
+                        //showActivity = new Intent(MainMenu.this, );
+                        break;
+                    case R.id.btnCurrentFootprint:
+                        //showActivity = new Intent(MainMenu.this, );
+                        break;
+                }
+                startActivity(showActivity);
             }
         });
     }
