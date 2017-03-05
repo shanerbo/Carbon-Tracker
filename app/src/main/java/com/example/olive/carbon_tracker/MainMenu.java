506979a0ff8428c73a08a0fb18f891b5530dc5ee
ncfung <ncfung@sfu.ca>
2017-03-04 16:44:16 -0800

    Add OnClickListener (Does Nothing)

--- app/src/main/java/com/example/olive/carbon_tracker/MainMenu.java
+++ app/src/main/java/com/example/olive/carbon_tracker/MainMenu.java
@@ -2,6 +2,8 @@ package com.example.olive.carbon_tracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainMenu extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class MainMenu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        setButton(R.id.btnCreateJourney);
+        setButton(R.id.btnSelectJourney);
+        setButton(R.id.btnCurrentFootprint);
     }
+
+    private void setButton(int id) {
+        Button button = (Button) findViewById(id);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+
 }
