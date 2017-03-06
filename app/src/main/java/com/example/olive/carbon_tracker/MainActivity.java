97a68c33b26d7cb0beb64e49b175c78f153795a0
shanerbo <shanerbo@gmail.com>
2017-03-06 13:16:14 -0800

    add welcome screen back ground image

--- app/src/main/java/com/example/olive/carbon_tracker/MainActivity.java
+++ app/src/main/java/com/example/olive/carbon_tracker/MainActivity.java
@@ -5,17 +5,23 @@ import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 
 import com.github.mikephil.charting.charts.PieChart;
 
 public class MainActivity extends AppCompatActivity {
-    private static int exist_time = 1000;
+    private static int exist_time = 2500;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        getSupportActionBar().hide();
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+
         setContentView(R.layout.activity_main);
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
