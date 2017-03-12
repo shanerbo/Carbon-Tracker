46c9c52d44f8533aaab302add996df5f55a9cfcb
ncfung <ncfung@sfu.ca>
2017-03-11 18:30:23 -0800

    Add new activities for editing/deleting

---
+++ app/src/main/java/com/example/olive/carbon_tracker/UI/DisplayJourneyList.java
@@ -0,0 +1,15 @@
+package com.example.olive.carbon_tracker.UI;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.olive.carbon_tracker.R;
+
+public class DisplayJourneyList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_journey_list);
+    }
+}
---
+++ app/src/main/java/com/example/olive/carbon_tracker/UI/EditJourney.java
@@ -0,0 +1,15 @@
+package com.example.olive.carbon_tracker.UI;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.olive.carbon_tracker.R;
+
+public class EditJourney extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_journey);
+    }
+}
