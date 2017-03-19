0ff9e6a4f7098b2fc582ffb18bd5142fc4edc3fa
Kyle <cca209@sfu.ca>
2017-03-19 13:34:44 -0700

    Set up Monthly Utilities layout

--- app/src/main/java/com/example/olive/carbon_tracker/UI/MonthlyUtilities.java
+++ app/src/main/java/com/example/olive/carbon_tracker/UI/MonthlyUtilities.java
@@ -1,7 +1,10 @@
 package com.example.olive.carbon_tracker.UI;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.olive.carbon_tracker.R;
 
@@ -11,5 +14,19 @@ public class MonthlyUtilities extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_monthly_utilities);
+
+        setupCalendarButton(R.id.ID_startDate_button);
+        setupCalendarButton(R.id.ID_endDate_button);
+
+    }
+
+    private void setupCalendarButton(int buttonID){
+        Button btn = (Button) findViewById(buttonID);
+        btn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MonthlyUtilities.this,Calendar.class);
+                startActivity(intent);
+            }
+        });
     }
 }
