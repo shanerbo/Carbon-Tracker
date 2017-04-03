e71ec275b7b47f21dda3b4b18f168c2dfeb7f867
ncfung <ncfung@sfu.ca>
2017-04-03 02:47:29 -0700

    Add version.xml
    Contains the current version number

--- app/src/main/java/com/example/olive/carbon_tracker/UI/AboutActivity.java
+++ app/src/main/java/com/example/olive/carbon_tracker/UI/AboutActivity.java
@@ -2,6 +2,7 @@ package com.example.olive.carbon_tracker.UI;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.olive.carbon_tracker.R;
 
@@ -11,5 +12,13 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        setTestView(R.id.txtVersion);
+    }
+
+    private void setTestView(int id) {
+        TextView textView = (TextView) findViewById(id);
+        float currentVersion = getResources().getDimension(R.dimen.current_version);
+        String msg = getString(R.string.app_version, currentVersion);
+        textView.setText(msg);
     }
 }
