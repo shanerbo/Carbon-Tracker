b28995533b8d6c28221020215a9c0e0b27080c0a
ncfung <ncfung@sfu.ca>
2017-04-05 16:56:43 -0700

    Fix bug in AboutActivity
    Now uses getValue() instead of getDimen()
    Add missing semi-colon

--- app/src/main/java/com/example/olive/carbon_tracker/UI/AboutActivity.java
+++ app/src/main/java/com/example/olive/carbon_tracker/UI/AboutActivity.java
@@ -2,7 +2,7 @@ package com.example.olive.carbon_tracker.UI;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.TypedValue
+import android.util.TypedValue;
 import android.widget.TextView;
 
 import com.example.olive.carbon_tracker.R;
@@ -19,7 +19,7 @@ public class AboutActivity extends AppCompatActivity {
     private void setTestView(int id) {
         TextView textView = (TextView) findViewById(id);
         TypedValue temp = new TypedValue();
-        getResources().getDimension(R.dimen.current_version, temp, true);
+        getResources().getValue(R.dimen.current_version, temp, true);
         float currentVersion = temp.getFloat();
         String msg = getString(R.string.app_version, currentVersion);
         textView.setText(msg);
